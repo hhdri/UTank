60d09e479134c90cafd6dfe97f22821aaa09950b
Majid Hajiheidari <majid.hheidari@gmail.com>
2020-02-11 22:28:39 +0330

    Fix merge conflicts

--- src/utank/GameActionListener.java
+++ src/utank/GameActionListener.java
@@ -24,6 +24,17 @@ public class GameActionListener extends KeyAdapter {
         this.number = number;
     }
 
+    public void resetVariables() {
+        this.p1Fire = false;
+        this.p1Right = false;
+        this.p1Left = false;
+        this.p1Move = false;
+        this.p2Fire = false;
+        this.p2Right = false;
+        this.p2Left = false;
+        this.p2Move = false;
+    }
+
     public void keyPressed(KeyEvent e) {
         switch (this.getNumber()) {
             case 1:
